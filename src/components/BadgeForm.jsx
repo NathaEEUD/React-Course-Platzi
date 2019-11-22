447fae293d1b5fc433250e166581488d1aed4e52
Nathaly <natharb09@gmail.com>
2019-11-22 18:30:56 -0500

    feat: state handle for new attendant form

--- src/components/BadgeForm.jsx
+++ src/components/BadgeForm.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 
 class BadgeForm extends Component {
+  state = {};
   handleChange = e => {
     console.log('input e::::', { name: e.target.name, value: e.target.value });
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
   };
 
   handleClick = e => {
@@ -26,6 +30,51 @@ class BadgeForm extends Component {
               className='form-control'
               type='text'
               name='firstName'
+              value={this.state.firstName}
+            />
+          </div>
+
+          <div className='form-group'>
+            <label>Last Name</label>
+            <input
+              onChange={this.handleChange}
+              className='form-control'
+              type='text'
+              name='lastName'
+              value={this.state.lastName}
+            />
+          </div>
+
+          <div className='form-group'>
+            <label>Email</label>
+            <input
+              onChange={this.handleChange}
+              className='form-control'
+              type='email'
+              name='email'
+              value={this.state.email}
+            />
+          </div>
+
+          <div className='form-group'>
+            <label>Job Title</label>
+            <input
+              onChange={this.handleChange}
+              className='form-control'
+              type='text'
+              name='jobTitle'
+              value={this.state.jobTitle}
+            />
+          </div>
+
+          <div className='form-group'>
+            <label>Twitter</label>
+            <input
+              onChange={this.handleChange}
+              className='form-control'
+              type='text'
+              name='twitter'
+              value={this.state.twitter}
             />
           </div>
 
