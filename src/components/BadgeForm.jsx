426d92a4a6584352fa9158a382586d4f6878bfa9
Nathaly <natharb09@gmail.com>
2019-11-23 20:30:12 -0500

    feat: state uprising for new attendant page

--- src/components/BadgeForm.jsx
+++ src/components/BadgeForm.jsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 class BadgeForm extends Component {
   state = {};
+
   handleChange = e => {
-    console.log('input e::::', { name: e.target.name, value: e.target.value });
     this.setState({
       [e.target.name]: e.target.value,
     });
@@ -26,55 +26,55 @@ class BadgeForm extends Component {
           <div className='form-group'>
             <label>First Name</label>
             <input
-              onChange={this.handleChange}
+              onChange={this.props.onChange}
               className='form-control'
               type='text'
               name='firstName'
-              value={this.state.firstName}
+              value={this.props.formValues.firstName}
             />
           </div>
 
           <div className='form-group'>
             <label>Last Name</label>
             <input
-              onChange={this.handleChange}
+              onChange={this.props.onChange}
               className='form-control'
               type='text'
               name='lastName'
-              value={this.state.lastName}
+              value={this.props.formValues.lastName}
             />
           </div>
 
           <div className='form-group'>
             <label>Email</label>
             <input
-              onChange={this.handleChange}
+              onChange={this.props.onChange}
               className='form-control'
               type='email'
               name='email'
-              value={this.state.email}
+              value={this.props.formValues.email}
             />
           </div>
 
           <div className='form-group'>
             <label>Job Title</label>
             <input
-              onChange={this.handleChange}
+              onChange={this.props.onChange}
               className='form-control'
               type='text'
               name='jobTitle'
-              value={this.state.jobTitle}
+              value={this.props.formValues.jobTitle}
             />
           </div>
 
           <div className='form-group'>
             <label>Twitter</label>
             <input
-              onChange={this.handleChange}
+              onChange={this.props.onChange}
               className='form-control'
               type='text'
               name='twitter'
-              value={this.state.twitter}
+              value={this.props.formValues.twitter}
             />
           </div>
 
--- src/pages/BadgeNew.jsx
+++ src/pages/BadgeNew.jsx
@@ -6,6 +6,25 @@ import Badge from '../components/Badge';
 import BadgeForm from '../components/BadgeForm';
 
 class BadgeNew extends Component {
+  state = {
+    form: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      jobTitle: '',
+      twitter: '',
+    },
+  };
+
+  handleChange = e => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
   render() {
     return (
       <div>
@@ -17,15 +36,16 @@ class BadgeNew extends Component {
           <div className='row'>
             <div className='col-6'>
               <Badge
-                firstName='Nathaly'
-                lastName='Riaño'
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                email={this.state.form.email}
+                jobTitle={this.state.form.jobTitle}
+                twitter={this.state.form.twitter}
                 avatarUrl='https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon'
-                jobTitle='Frontend Engineer'
-                twitter='nasterboost'
               />
             </div>
             <div className='col-6'>
-              <BadgeForm />
+              <BadgeForm onChange={this.handleChange} formValues={this.state.form} />
             </div>
           </div>
         </div>
